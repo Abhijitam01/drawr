2ddb2e2fe44e39a5ea7186a9ba47794c597e7888
Abhijitam01 <abhijitamdubey31@gmail.com>
2025-12-23 23:01:29 +0530

    starting witht he backend

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,6 +2,16 @@ import express from 'express';
 
 const app = express();
 
+app.post("/signup",(req , res) => {
+
+})
+app.post("/signin",(req , res) => {
+
+})
+app.post("/room",(req , res) => {
+
+})
+
 app.listen(3000, () => {
   console.log('HTTP server is running on port 3000');
 });
